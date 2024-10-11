b7dc039fcf5c3149cba2037b5aa28878d7727e5f
samussouza <samuel.ssouza@sptech.school>
2024-10-11 17:12:26 -0300

    Adionando download de arquivos

--- apache-poi-dataTech/src/main/java/Main.java
+++ apache-poi-dataTech/src/main/java/Main.java
@@ -6,9 +6,10 @@ public class Main {
     public static void main(String[] args) {
         S3Service s3Service = new S3Service(new S3Provider().getS3Client(), "bucket-data-tech");
 
-        s3Service.createBucket();
-        s3Service.uploadFiles("C:/Users/sdssd/Desktop/file.txt");
+
+        /*s3Service.uploadFiles("C:/Users/sdssd/Desktop/file.txt");*/
         s3Service.listObjects();
+        s3Service.downloadFiles();
     }
 
 }
