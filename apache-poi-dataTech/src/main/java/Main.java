272ab7af43cd8a3b0a58fe18318ccec85b554578
PajorVi <vinicius.marques@sptech.school>
2024-10-11 17:09:58 -0300

    organizando estrutura de pastas e fazendo conexão com banco

--- apache-poi-dataTech/src/main/java/Main.java
+++ apache-poi-dataTech/src/main/java/Main.java
@@ -1,3 +1,6 @@
+import org.springframework.jdbc.core.JdbcTemplate;
+import writer.ConexaoBanco;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -5,6 +8,13 @@ public class Main {
         Log logs = new Log();
         logs.mostrarLogs();
 
+        ConexaoBanco conexaoBanco = new ConexaoBanco();
+        JdbcTemplate connection = conexaoBanco.getConnection();
+
+        connection.update("""
+                INSERT INTO dataTech.usuario (cpf, nome, email, senha, fk_empresa) values
+                ("12345678901", "teste-conexao", "teste@gmail", "minhasenha123", 1);
+                """);
     }
 
 }
